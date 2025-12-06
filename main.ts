ad7d18463782e496c3b792661950878532ed658f
Chase Reynolds <30632225+itsagift@users.noreply.github.com>
2025-12-06 14:07:13 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,21 @@
-basic.showIcon(IconNames.Heart)
+/**
+ * 1. Make a prediction about what "show string Hello" will do. Write it on your worksheet.
+ * 
+ * 2. Replace the heart icon with the string "Hello"
+ * 
+ * 3. Download the code and make sure it runs on your Microbit device. Does it look the same as the simulator?
+ * 
+ * 4. Change the string to your name 
+ * 
+ * 5. Download your code again and run it on your Microbit device.
+ * 
+ * 6. Choose one of the images on the screen. Try to represent it using the 25 Microbit LEDs. Use the planning sheet to brainstorm before running your code. 
+ * 
+ * 7. Download your completed image to your Microbit device and share your code to the classroom.
+ */
+/**
+ * This is a comment in makecode. It may look similar to ones you've seen in Scratch.
+ */
 basic.forever(function () {
 	
 })
